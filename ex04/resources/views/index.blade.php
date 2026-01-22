ec919d863dab0f7c72879388942670ef12b7f95b
Rayco-jpg <gutierrezrayco13@gmail.com>
2026-01-22 12:26:53 +0100

    crear el listado de prendas

---
+++ ex04/resources/views/index.blade.php
@@ -0,0 +1,10 @@
+<nav>
+    <ul>
+        <li>
+            <a href="{{ route('prendas.index') }}">Listado de prendas</a>
+        </li>
+        <li>
+            <a href="{{ route('prendas.create') }}">Creación de prenda</a>
+        </li>
+    </ul>
+</nav>
